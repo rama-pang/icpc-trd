dd490e4f6d9d94a5b14defa5089850f8c44b5e32
user202729 <25191436+user202729@users.noreply.github.com>
2023-09-05 18:47:54 +0700

    Add KACTL

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # icpc-trd
 ICPC 2023
+
+Taken from https://github.com/kth-competitive-programming/kactl, with modifications.
--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -1,58 +1,55 @@
+#if not LOCAL
+#define NDEBUG 1
+#endif
 #include <bits/stdc++.h>
 using namespace std;
 
+#define rep(i, a, b) for(auto i = a; i < (b); ++i)
+#define down(x, a) for (auto x = a; x--;)
+#define all(x) begin(x), end(x)
+#define sz(x) int(size(x))
+#define let auto const
+
 using ll = long long;
-using lint = long long;
+using lint = ll;
 using pii = pair<int, int>;
 using vi = vector<int>;
 
-template <class F>
-class YComb_ {
-  F f_;
- public:
-  template <class T>
-  explicit YComb_(T&& f) : f_(forward<T>(f)) {}
-  template <class... Args>
-  decltype(auto) operator()(Args&&... args) {
-    return f_(ref(*this), forward<Args>(args)...);
-  }
-};
-
-template <class F>
-decltype(auto) YComb(F&& f) {
-  return YComb_<decay_t<F>>(forward<F>(f));
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	cin.exceptions(cin.failbit);
 }
 
-#define let auto const
+// extra
 
-#define repeat(x, a, b) for (auto x = a; x < (b); x++)
-#define down(x, a) for (auto x = a; x--;)
-#define len(x) int(size(x))
-#define all(x) begin(x), end(x)
-
-#define rep repeat
-#define sz len
+template<class T> struct YComb_{
+	T f;
+	template<class... Args> auto operator()(Args&&... args)const{
+		return f(ref(*this), forward<Args>(args)...);
+	}
+};
+template<class T> YComb_<T> YComb(T f){ return {move(f)}; }
 
 template <class T>
 auto& operator>>(istream& s, vector<T>& v) {
-  for (auto& x : v) s >> x;
-  return s;
+	for (auto& x : v) s >> x;
+	return s;
 }
 
 template <class A, class B>
 auto& operator>>(istream& s, pair<A, B>& v) {
-  return s >> v.first >> v.second;
+	return s >> v.first >> v.second;
 }
 
 template <class T>
 auto& operator<<(ostream& s, vector<T> const& v) {
-  for (auto& x : v) s << x << ' ';
-  return s;
+	for (auto& x : v) s << x << ' ';
+	return s;
 }
 
 template <class A, class B>
 auto& operator<<(ostream& s, pair<A, B> const& v) {
-  return s << v.first << ' ' << v.second;
+	return s << v.first << ' ' << v.second;
 }
 
 #define lambda(x, y) [&](let& x) { return (y); }
@@ -61,17 +58,10 @@ auto& operator<<(ostream& s, pair<A, B> const& v) {
 #define apply(f, v, e...) ([&] { let _tmp = v; return f(all(_tmp), e); }())
 
 auto iota(int l, int r) {
-  assert(l <= r);
-  vector<int> a(r - l);
-  iota(all(a), l);
-  return a;
+	assert(l <= r);
+	vector<int> a(r - l);
+	iota(all(a), l);
+	return a;
 }
 
 auto iota(int r) { return iota(0, r); }
-
-signed main() {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  
-  
-}
