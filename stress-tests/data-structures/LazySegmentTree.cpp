855865373a6322dc1911f36d423954c97a794350
user202729 <25191436+user202729@users.noreply.github.com>
2023-09-20 15:13:07 +0700

    Update

--- stress-tests/data-structures/LazySegmentTree.cpp
+++ stress-tests/data-structures/LazySegmentTree.cpp
@@ -2,19 +2,13 @@
 
 #include "../../content/data-structures/LazySegmentTree.h"
 
-static unsigned R;
-int ra() {
-	R *= 791231;
-	R += 1231;
-	return (int)(R >> 1);
-}
-
 volatile int res;
 int main() {
 	int N = 10;
 	vi v(N);
 	iota(all(v), 0);
-	random_shuffle(all(v), [](int x) { return ra() % x; });
+	mt19937 engine;
+	shuffle(all(v), engine);
 	Node* tr = new Node(v,0,N);
 	rep(i,0,N) rep(j,0,N) if (i <= j) {
 		int ma = -inf;
@@ -22,11 +16,11 @@ int main() {
 		assert(ma == tr->query(i,j));
 	}
 	rep(it,0,1000000) {
-		int i = ra() % (N+1), j = ra() % (N+1);
+		int i = uniform_int_distribution(0, N)(engine), j=uniform_int_distribution(0, N)(engine);
 		if (i > j) swap(i, j);
-		int x = (ra() % 10) - 5;
+		int x = uniform_int_distribution(-5, 4)(engine);
 
-		int r = ra() % 100;
+		int r = uniform_int_distribution(0, 99)(engine);
 		if (r < 30) {
 			::res = tr->query(i, j);
 			int ma = -inf;
