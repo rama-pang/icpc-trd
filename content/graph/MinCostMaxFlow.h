d3c47ba8548546ebd42fbfd1197b1b342369ad80
Amar <nvmdava@gmail.com>
2023-10-16 19:18:06 +0800

    mcmf O(M) memory Halim cp-book)

--- content/graph/MinCostMaxFlow.h
+++ content/graph/MinCostMaxFlow.h
@@ -1,94 +1,90 @@
-/**
- * Author: Stanford
- * Date: Unknown
- * Source: Stanford Notebook
- * Description: Min-cost max-flow. cap[i][j] != cap[j][i] is allowed; double edges are not.
- *  If costs can be negative, call setpi before maxflow, but note that negative cost cycles are not supported.
- *  To obtain the actual flow, look at positive values only.
- * Status: Tested on kattis:mincostmaxflow, stress-tested against another implementation
- * Time: Approximately O(E^2)
- */
-#pragma once
+// This code uses new C++17 structured binding
+// use this compiler setting "g++ -O2 -std=gnu++17 {cpp17file}"
 
-// #include <bits/extc++.h> /// include-line, keep-include
+// Disclaimer: This code is a hybrid between old CP1-2-3 implementation of
+// Edmonds Karp's algorithm -- re-written in OOP fashion and the fast
+// Dinic's algorithm implementation by
+// https://github.com/jaehyunp/stanfordacm/blob/master/code/Dinic.cc
+// This code is written in modern C++17 standard
 
-const ll INF = numeric_limits<ll>::max() / 4;
-typedef vector<ll> VL;
+// We replace BFS with SPFA
 
-struct MCMF {
-	int N;
-	vector<vi> ed, red;
-	vector<VL> cap, flow, cost;
-	vi seen;
-	VL dist, pi;
-	vector<pii> par;
 
-	MCMF(int N) :
-		N(N), ed(N), red(N), cap(N, VL(N)), flow(cap), cost(cap),
-		seen(N), dist(N), pi(N), par(N) {}
+typedef tuple<int, ll, ll, ll> edge;
+typedef vector<ll> vll;
 
-	void addEdge(int from, int to, ll cap, ll cost) {
-		assert(!this->cap[from][to]); // double edge not supported
-		this->cap[from][to] = cap;
-		this->cost[from][to] = cost;
-		ed[from].push_back(to);
-		red[to].push_back(from);
-	}
+const ll INF = 1e18; // INF = 1e18, not 2^63-1 to avoid overflow
 
-	void path(int s) {
-		fill(all(seen), 0);
-		fill(all(dist), INF);
-		dist[s] = 0; ll di;
+class min_cost_max_flow {
+private:
+  int V;
+  ll total_cost;
+  vector<edge> EL;
+  vector<vi> AL;
+  vll d;
+  vi last, vis;
 
-		__gnu_pbds::priority_queue<pair<ll, int>> q;
-		vector<decltype(q)::point_iterator> its(N);
-		q.push({0, s});
+  bool SPFA(int s, int t) { // SPFA to find augmenting path in residual graph
+    d.assign(V, INF); d[s] = 0; vis[s] = 1;
+    queue<int> q({s});
+    while (!q.empty()) {
+      int u = q.front(); q.pop(); vis[u] = 0;
+      for (auto &idx : AL[u]) {                  // explore neighbors of u
+        auto &[v, cap, flow, cost] = EL[idx];          // stored in EL[idx]
+        if ((cap-flow > 0) && (d[v] > d[u] + cost)) {      // positive residual edge
+          d[v] = d[u]+cost;
+          if(!vis[v]) q.push(v), vis[v] = 1;
+        }
+      }
+    }
+    return d[t] != INF;                           // has an augmenting path
+  }
 
-		auto relax = [&](int i, ll cap, ll cost, int dir) {
-			ll val = di - pi[i] + cost;
-			if (cap && val < dist[i]) {
-				dist[i] = val;
-				par[i] = {s, dir};
-				if (its[i] == q.end()) its[i] = q.push({-dist[i], i});
-				else q.modify(its[i], {-dist[i], i});
-			}
-		};
+  ll DFS(int u, int t, ll f = INF) {             // traverse from s->t
+    if ((u == t) || (f == 0)) return f;
+    vis[u] = 1;
+    for (int &i = last[u]; i < (int)AL[u].size(); ++i) { // from last edge
+      auto &[v, cap, flow, cost] = EL[AL[u][i]];
+      if (!vis[v] && d[v] == d[u]+cost) {                      // in current layer graph
+        if (ll pushed = DFS(v, t, min(f, cap-flow))) {
+      total_cost += pushed * cost;
+          flow += pushed;
+          auto &[rv, rcap, rflow, rcost] = EL[AL[u][i]^1]; // back edge
+          rflow -= pushed;
+          vis[u] = 0;
+          return pushed;
+        }
+      }
+    }
+    vis[u] = 0;
+    return 0;
+  }
 
-		while (!q.empty()) {
-			s = q.top().second; q.pop();
-			seen[s] = 1; di = dist[s] + pi[s];
-			for (int i : ed[s]) if (!seen[i])
-				relax(i, cap[s][i] - flow[s][i], cost[s][i], 1);
-			for (int i : red[s]) if (!seen[i])
-				relax(i, flow[i][s], -cost[i][s], 0);
-		}
-		rep(i,0,N) pi[i] = min(pi[i] + dist[i], INF);
-	}
+public:
+  min_cost_max_flow(int initialV) : V(initialV), total_cost(0) {
+    EL.clear();
+    AL.assign(V, vi());
+    vis.assign(V, 0);
+  }
 
-	pair<ll, ll> maxflow(int s, int t) {
-		ll totflow = 0, totcost = 0;
-		while (path(s), seen[t]) {
-			ll fl = INF;
-			for (int p,r,x = t; tie(p,r) = par[x], x != s; x = p)
-				fl = min(fl, r ? cap[p][x] - flow[p][x] : flow[x][p]);
-			totflow += fl;
-			for (int p,r,x = t; tie(p,r) = par[x], x != s; x = p)
-				if (r) flow[p][x] += fl;
-				else flow[x][p] -= fl;
-		}
-		rep(i,0,N) rep(j,0,N) totcost += cost[i][j] * flow[i][j];
-		return {totflow, totcost};
-	}
+  // if you are adding a bidirectional edge u<->v with weight w into your
+  // flow graph, set directed = false (default value is directed = true)
+  void add_edge(int u, int v, ll w, ll c, bool directed = true) {
+    if (u == v) return;                          // safeguard: no self loop
+    EL.emplace_back(v, w, 0, c);                 // u->v, cap w, flow 0, cost c
+    AL[u].push_back(EL.size()-1);                // remember this index
+    EL.emplace_back(u, 0, 0, -c);                // back edge
+    AL[v].push_back(EL.size()-1);                // remember this index
+    if (!directed) add_edge(v, u, w, c);         // add again in reverse
+  }
 
-	// If some costs can be negative, call this before maxflow:
-	void setpi(int s) { // (otherwise, leave this out)
-		fill(all(pi), INF); pi[s] = 0;
-		int it = N, ch = 1; ll v;
-		while (ch-- && it--)
-			rep(i,0,N) if (pi[i] != INF)
-				for (int to : ed[i]) if (cap[i][to])
-					if ((v = pi[i] + cost[i][to]) < pi[to])
-						pi[to] = v, ch = 1;
-		assert(it >= 0); // negative cost cycle
-	}
+  pair<ll, ll> mcmf(int s, int t) {
+    ll mf = 0;                                   // mf stands for max_flow
+    while (SPFA(s, t)) {                          // an O(V^2*E) algorithm
+      last.assign(V, 0);                         // important speedup
+      while (ll f = DFS(s, t))                   // exhaust blocking flow
+        mf += f;
+    }
+    return {mf, total_cost};
+  }
 };
