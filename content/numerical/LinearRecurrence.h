9487a87a78e2f27caeed366f5f28657689ee5826
user202729 <25191436+user202729@users.noreply.github.com>
2023-10-16 10:09:32 +0700

    Fast LinearRecurrence

--- content/numerical/LinearRecurrence.h
+++ content/numerical/LinearRecurrence.h
@@ -12,43 +12,44 @@
  * Usage: linearRec({0, 1}, {1, 1}, p) // p'th Fibonacci number
  * Time: O(n^2 \log p)
  * Status: bruteforce-tested mod 5 for n <= 5, tested on https://www.spoj.com/problems/ITRIX12E/
+ *  tested on https://contest.ucup.ac/contest/1356/problem/7187
  */
 #pragma once
 
 #include "../number-theory/ModularArithmetic.h"
 #include "NumberTheoreticTransform.h"
 
-#define FAST 1
-
 Mod linearRec(vm S, vm tr, ll p) {
 	int n = sz(tr);
 	assert(sz(S)==n);
 
-#if FAST
+#if not SLOW
 	auto m=tr;
 	for(auto& x: m) x=-x;
+	reverse(all(m));
 	m.push_back(1);
 #endif
 
 	auto combine = [&](vm a, vm b) {
-#if FAST
-		let r=conv(move(a), move(b));
-		return polymod(r, m, polydiv(r, m));
-#else
-		vm res(n * 2 + 1);
+#if SLOW
+		vm res(n*2-1);
 		rep(i,0,sz(a)) rep(j,0,sz(b))
 			res[i + j] += a[i] * b[j];
-		for (int i = 2 * n; i > n; --i) rep(j,0,n)
-			res[i - 1 - j] += res[i] * tr[j];
-		res.resize(n + 1);
+		while(sz(res)>n){
+			rep(j,0,n) res.end()[-j-2] += res.back() * tr[j];
+			res.pop_back();
+		}
 		return res;
+#else
+		let r=conv(move(a), move(b));
+		return polymod(r, m, polydiv(r, m));
 #endif
 	};
 
 	vm pol{1}, e{0, 1};
-	for (++p; p; p>>=1, e=combine(e, e)) if (p&1) pol = combine(move(pol), e);
+	for (; p; p>>=1, e=combine(e, e)) if (p&1) pol = combine(move(pol), e);
 
 	Mod res = 0;
-	rep(i,0,sz(pol)-1) res += pol[i + 1] * S[i];
+	rep(i,0,sz(pol)) res += pol[i] * S[i];
 	return res;
 }
