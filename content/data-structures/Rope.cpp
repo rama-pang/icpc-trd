86c0471d08f8851bb21b73c1707c91772de223a7
user202729 <25191436+user202729@users.noreply.github.com>
2023-10-21 09:12:24 +0700

    Move things around, add rope

---
+++ content/data-structures/Rope.cpp
@@ -0,0 +1,12 @@
+#include<ext/rope>
+using namespace __gnu_cxx;
+rope<int> v(3, 0); // initial list not supported
+vector<int> a{1, 2, 3, 4};
+rope<int> w(a.data(), sz(a));
+v.push_back(3);
+assert(sz(v.substr(1, 2))==2);
+v.erase(1, 2);
+assert(sz(v)==2);
+rope<int> cur=v.substr(0, 1);
+v.insert(0, cur);
+for(auto x: v) return x;
