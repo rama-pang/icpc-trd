8dba4fabfde31411b8b6720b479d711f6984d045
user202729 <25191436+user202729@users.noreply.github.com>
2023-10-22 17:31:30 +0700

    Delete some easy geometry things

--- content/geometry/Point.h
+++ content/geometry/Point.h
@@ -9,14 +9,14 @@
  */
 #pragma once
 
-template <class T> int sgn(T x) { return (x > 0) - (x < 0); }
+int sgn(auto x) { return (x > 0) - (x < 0); }
 template<class T>
 struct Point {
-	typedef Point P;
+	using P=Point;
 	T x, y;
-	explicit Point(T x=0, T y=0) : x(x), y(y) {}
-	bool operator<(P p) const { return tie(x,y) < tie(p.x,p.y); }
-	bool operator==(P p) const { return tie(x,y)==tie(p.x,p.y); }
+#define C(O) bool operator O(P p) const { return tie(x,y) O tie(p.x,p.y); }
+	C(<) C(==)
+#undef C
 	P operator+(P p) const { return P(x+p.x, y+p.y); }
 	P operator-(P p) const { return P(x-p.x, y-p.y); }
 	P operator*(T d) const { return P(x*d, y*d); }
@@ -24,13 +24,15 @@ struct Point {
 	T dot(P p) const { return x*p.x + y*p.y; }
 	T cross(P p) const { return x*p.y - y*p.x; }
 	T cross(P a, P b) const { return (a-*this).cross(b-*this); }
-	T dist2() const { return x*x + y*y; }
-	double dist() const { return sqrt((double)dist2()); }
+	tuple<bool, T, T> decompose(P a, P b) const {
+		let d=a.cross(b); if(d==0) return {};
+		return {a.cross(*this)/d, cross(b)/d};
+	}
+	T norm() const { return x*x + y*y; }
+	double len() const { return sqrt(norm()); }
 	// angle to x-axis in interval [-pi, pi]
 	double angle() const { return atan2(y, x); }
-	P unit() const { return *this/dist(); } // makes dist()=1
 	P perp() const { return P(-y, x); } // rotates +90 degrees
-	P normal() const { return perp().unit(); }
 	// returns point rotated 'a' radians ccw around the origin
 	P rotate(double a) const {
 		return P(x*cos(a)-y*sin(a),x*sin(a)+y*cos(a)); }
