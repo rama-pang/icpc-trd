6bda7e52ff757dec871e479823e6ed3f4c62cfd4
user202729 <25191436+user202729@users.noreply.github.com>
2023-10-23 21:55:24 +0700

    Shorten lazy segment tree implementation

--- content/data-structures/LazySegmentTree.h
+++ content/data-structures/LazySegmentTree.h
@@ -16,47 +16,41 @@
 #include "../various/BumpAllocator.h"
 
 const int inf = 1e9;
-struct Op {
-	int set=inf, add=0;
-}; // add * set * value
+struct Op { int set=inf, add=0; }; // in this example set is applied first, then add
 Op const Identity {};
 Op operator*(Op a, Op b){
-	if(a.set==inf){
-		b.add+=a.add;
-		return b;
-	}
+	if(a.set==inf) return b.add+=a.add, b;
 	return a;
 }
 int operator*(Op mod, int val){
-	if(mod.set!=inf) val=mod.set;
-	return mod.add+val;
+	if(mod.set!=inf){val=mod.set;} return mod.add+val;
 }
+#define mergeVal max
+let emptyVal=-inf;  // * identity of mergeVal
 struct Node {
 	Node *l = 0, *r = 0;
 	int lo, hi;
-	Op mod=Identity;
-	int val = -inf;
-	Node(int lo,int hi):lo(lo),hi(hi){} // Large interval of -inf
-	Node(vi& v, int lo, int hi) : lo(lo), hi(hi) {
+	Op mod=Identity; int val = emptyVal;
+	Node(int lo,int hi):lo(lo),hi(hi){} // Large empty interval
+	Node(vi& v, int lo, int hi) : lo(lo), hi(hi) { // Initialize from segment of v
 		if (lo + 1 < hi) {
 			int mid = lo + (hi - lo)/2;
 			l = new Node(v, lo, mid); r = new Node(v, mid, hi);
-			val = max(l->val, r->val);
-		}
-		else val = v[lo];
+			val = mergeVal(l->val, r->val);
+		} else val = v[lo]; // * set default value
 	}
 	int query(int L, int R) {
-		if (R <= lo || hi <= L) return -inf;
+		if (R <= lo || hi <= L) return emptyVal;
 		if (L <= lo && hi <= R) return val;
 		push();
-		return max(l->query(L, R), r->query(L, R));
+		return mergeVal(l->query(L, R), r->query(L, R));
 	}
 	void op(int L, int R, Op x) {
 		if (R <= lo || hi <= L) return;
 		if (L <= lo && hi <= R) mod = x * mod, val = x * val;
 		else {
 			push(), l->op(L, R, x), r->op(L, R, x);
-			val = max(l->val, r->val);
+			val = mergeVal(l->val, r->val);
 		}
 	}
 	void set(int L, int R, int x) {
