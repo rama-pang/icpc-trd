17a058cb32537012b323ed25d8db9510e734a11b
user202729 <25191436+user202729@users.noreply.github.com>
2023-11-07 08:15:41 +0700

    Add FT3???

--- content/data-structures/FenwickTree2d.h
+++ content/data-structures/FenwickTree2d.h
@@ -6,7 +6,7 @@
  * Description: Computes sums $a[i,j]$ for all $i<I$, $j<J$, and increases single elements $a[i,j]$.
  *  Requires that the elements to be updated are known in advance (call fakeUpdate() before init()).
  * Time: $O(\log^2 N)$. (Use persistent segment trees for $O(\log N)$.)
- * Status: stress-tested
+ * Status: stress-tested (except FT3 which is tested once at 1106-jak-regional/F_old.cpp but that one is TLE)
  */
 #pragma once
 
@@ -33,4 +33,15 @@ struct FT2 {
 			sum += ft[x-1].query(ind(x-1, y));
 		return sum;
 	}
+};
+
+struct FT3{ // also with x-coordinate compression
+	vector<int> xs;FT2 f={0};
+	void fakeUpdate1(int x){xs.push_back(x);}
+	void init(){ sort(all(xs)),f={sz(xs)}; }
+	void fakeUpdate2(int x,int y){ f.fakeUpdate(ind(x),y); }
+	// then call ft.f.init();
+	void update(int x,int y,int dif){ f.update(ind(x),y,dif); }
+	int query(int x,int y){ return f.query(ind(x),y); }
+	int ind(int x){ return int(lower_bound(all(xs),x)-xs.begin());}
 };
