2045b63c78e36b10c0b297e8dd42af674e6ebc32
user202729 <25191436+user202729@users.noreply.github.com>
2023-11-18 20:33:38 +0700

    Use FiraMono font

--- content/graph/2sat.h
+++ content/graph/2sat.h
@@ -4,12 +4,12 @@
  * License: CC0
  * Source: folklore
  * Description: Calculates a valid assignment to boolean variables a, b, c,... to a 2-SAT problem, so that an expression of the type $(a\|\|b)\&\&(!a\|\|c)\&\&(d\|\|!b)\&\&...$ becomes true, or reports that it is unsatisfiable.
- * Negated variables are represented by bit-inversions (\texttt{\tilde{}x}).
+ * Negated variables are represented by bit-inversions (\texttt{\~{}x}).
  * Usage:
  *  TwoSat ts(number of boolean variables);
- *  ts.either(0, \tilde3); // Var 0 is true or var 3 is false
+ *  ts.either(0, \~{}3); // Var 0 is true or var 3 is false
  *  ts.setValue(2); // Var 2 is true
- *  ts.atMostOne({0,\tilde1,2}); // <= 1 of vars 0, \tilde1 and 2 are true
+ *  ts.atMostOne({0,\~{}1,2}); // <= 1 of vars 0, \~{}1 and 2 are true
  *  ts.solve(); // Returns true iff it is solvable
  *  ts.values[0..N-1] holds the assigned values to the vars
  * Time: O(N+E), where N is the number of boolean variables, and E is the number of clauses.
