c12a2b463c42ae54284f068564c557c3c02055d5
user202729 <25191436+user202729@users.noreply.github.com>
2023-11-23 14:45:36 +0700

    Some polish

--- content/various/FastKnapsack.h
+++ content/various/FastKnapsack.h
@@ -2,8 +2,8 @@
  * Author: Mårten Wiman
  * License: CC0
  * Source: Pisinger 1999, "Linear Time Algorithms for Knapsack Problems with Bounded Weights"
- * Description: Given N non-negative integer weights w and a non-negative target t,
- * computes the maximum S <= t such that S is the sum of some subset of the weights.
+ * Description: Given $N$ non-negative integer weights $w$ and a non-negative target $t$,
+ * computes the maximum $S \le t$ such that $S$ is the sum of some subset of the weights.
  * Time: O(N \max(w_i))
  * Status: Tested on kattis:eavesdropperevasion, stress-tested
  */
