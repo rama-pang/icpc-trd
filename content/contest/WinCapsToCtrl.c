e9a5fe4df77a634fedeccae813afbb93a6e71b64
user202729 <25191436+user202729@users.noreply.github.com>
2023-12-16 23:46:15 +0700

    Win caps to control

---
+++ content/contest/WinCapsToCtrl.c
@@ -0,0 +1,11 @@
+/**
+ * Author: user202729
+ * Description: Maps Caps Lock to Ctrl.
+ */
+#include<windows.h>
+LRESULT f(int n,WPARAM w,LPARAM l) {
+	return n>=0&&w<258&&*(int*)l==20 ? keybd_event(17,0,w%2*2,0),1 : CallNextHookEx(0,n,w,l); }
+int main() {
+	SetWindowsHookEx(13,f,GetModuleHandle(0),0);
+	MSG m; while(GetMessage(&m,0,0,0))
+		TranslateMessage(&m),DispatchMessage(&m);}
