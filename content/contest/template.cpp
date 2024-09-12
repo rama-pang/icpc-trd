934bfe1a7422e151749b9141471bfeea6a142d6f
Rama Aryasuta Pangestu <rama.a.pangestu@gmail.com>
2024-09-12 17:40:45 +0800

    Optimize reroot template

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -3,27 +3,28 @@
 #endif
 #include <bits/stdc++.h>
 using namespace std;
+namespace R = ranges;
+namespace V = views;
+
 #define rep(i, a, b) for (auto i = a; i < (b); ++i)
 #define down(x, a) for (auto x = a; x--;)
-#define all(x) begin(x), end(x)
-#define sz(x) int(size(x))
+#define all(x) R::begin(x), R::end(x)
+#define sz(x) int(R::size(x))
 #define let auto const
+
 using ll = long long;
 using lint = ll;
 using pii = pair<int, int>;
 using vi = vector<int>;
-int main() {
-  cin.tie(0)->sync_with_stdio(0);
-  cin.exceptions(cin.failbit); }
-template <class T> struct YComb { T f; 
+
+template <class T> struct YComb { T f;
 	template <class... A> auto operator()(A&&... a) const {
     return f(ref(*this), forward<A>(a)...); } };
-auto iota(auto l, auto r) { return views::iota(l, r); }
-auto iota(auto r) { return views::iota(0, r); }
-template <class T>
-requires ranges::range<T>
+auto iota(auto l, auto r) { return V::iota(l, r); }
+auto iota(auto r) { return V::iota(0, r); }
+template <R::range T>
 auto& operator>>(istream& s, T&& v) {
-  ranges::for_each(v, [&](auto&& x) { s >> x; });
+  R::for_each(v, [&](auto&& x) { s >> x; });
   return s; }
 template <class A, class B>
 auto& operator>>(istream& s, pair<A, B>& v) {
@@ -32,10 +33,9 @@ template <class... A>
 auto& operator>>(istream& s, tuple<A...>& v) {
   apply([&](auto&... x) { ((s >> x), ...); }, v);
   return s; }
-template <class T>
-requires ranges::range<T>
+template <R::range T>
 auto& operator<<(ostream& s, T&& v) {
-  ranges::for_each(v, [&](auto&& x) { s << x << ' '; });
+  R::for_each(v, [&](auto&& x) { s << x << ' '; });
   return s; }
 template <class A, class B>
 auto& operator<<(ostream& s, pair<A, B> const& v) {
@@ -44,7 +44,8 @@ template <class... A>
 auto& operator<<(ostream& s, tuple<A...> const& v) {
   apply([&](auto&... x) { ((s << x << ' '), ...); }, v);
   return s; }
-#define lambda(x, y) [&](let& x) { return (y); }
-#define lambda2(x, y, z) [&](let& x, let& y) { return (z); }
-#define compare_by(e) lambda2(x, y, lambda(it, e)(x) < lambda(it, e)(y))
-#define apply(f, v, e...) ([&] { let _tmp = v; return f(all(_tmp), e); }())
+
+int main() {
+  cin.tie(0)->sync_with_stdio(0);
+  cin.exceptions(cin.failbit);
+}
--- content/graph/AllDirectionTreeDP.h
+++ content/graph/AllDirectionTreeDP.h
@@ -1,46 +1,38 @@
 /**
  * Author: koosaga
- * Description: Calculates DP value for all roots. Need to implement four functions:
- * (1) E: identity 
- * (2) take\_vertex: add vertex on top of merged edges
- * (3) up\_root: update child DP to consider parent edge values
- * (4) merge: merge two child edges
+ * Description: Calculates DP value for all roots. Need to implement three functions:
+ * (1) take\_vertex: add vertex on top of merged edges
+ * (2) up\_root: update child DP to consider parent edge values
+ * (3) merge: merge two child edges
  * It's good if merges are commutative (its not necessary but be careful of specifics)
  * Time: O(N)
  * Status: hopefully safe.
  */
-elem E() { return elem{Mod(0), Mod(0)}; }
-elem take_vertex(elem DP, int v) { return elem{DP[0] + a[v], DP[1] + Mod(1)}; }
-elem up_root(elem DP, int e) { return elem{DP[0] * b[e] + DP[1] * c[e], DP[1]}; }
-elem merge(elem a, elem b) { return elem{a[0] + b[0], a[1] + b[1]}; }
-void dfs(int x, vector<vector<pi>> &adj, vi &ord, vi &pae) {
-  ord.push_back(x);
-  for (auto [i, y] : adj[x]) {
-    adj[y].erase(find(all(adj[y]), pi{i ^ 1, x}));
-    pae[y] = (i ^ 1);
-    dfs(y, adj, ord, pae);
-  }
-}
-vector<elem> solve(int n, vector<pi> edges) {
-  vector<vector<pi>> adj(n);
+template<class T, auto take_vertex, auto up_root, auto merge>
+vector<T> solve(int n, vector<pii> edges) {
+  vector<vector<pii>> adj(n);
   rep(i, 0, n-1) {
     auto [u, v] = edges[i];
     adj[u].push_back({2 * i, v});
     adj[v].push_back({2 * i + 1, u});
   }
   vi ord, pae(n, -1);
-  dfs(0, adj, ord, pae);
-  vector<elem> dp(n, E()), rev_dp(n, E()), sol(n, E());
-  reverse(all(ord));
-  for (auto z : ord) {
+  YComb([&](auto self, int x) -> void {
+    ord.push_back(x);
+    for (auto [i, y] : adj[x]) {
+      adj[y].erase(find(all(adj[y]), pii{i ^ 1, x}));
+      pae[y] = i ^ 1; self(y);
+    }
+  })(0);
+  vector<T> dp(n), rev_dp(n), sol(n);
+  for (auto z : V::reverse(ord)) {
     for (auto [i, y] : adj[z]) {
       dp[z] = merge(dp[z], up_root(dp[y], i));
     }
     dp[z] = take_vertex(dp[z], z);
   }
-  reverse(all(ord));
   for (auto z : ord) {
-    vector<elem> pref(sz(adj[z]) + 1, E()), suff(sz(adj[z]) + 1, E());
+    vector<T> pref(sz(adj[z]) + 1), suff(sz(adj[z]) + 1);
     if (~pae[z]) pref[0] = up_root(rev_dp[z], pae[z]);
     rep(i, 0, sz(adj[z])) {
       auto [e, y] = adj[z][i];
@@ -60,3 +52,6 @@ vector<elem> solve(int n, vector<pi> edges) {
   }
   return sol;
 }
+elem take_vertex(elem DP, int v) { return elem{DP[0] + a[v], DP[1] + Mod(1)}; }
+elem up_root(elem DP, int e) { return elem{DP[0] * b[e] + DP[1] * c[e], DP[1]}; }
+elem merge(elem a, elem b) { return elem{a[0] + b[0], a[1] + b[1]}; }
